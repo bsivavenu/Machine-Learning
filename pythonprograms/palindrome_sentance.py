2be2c3c377dac09514b8e583a4984a24fb8d9853
bsivavenu <blue.black83@gmail.com>
2018-03-24 04:20:35 +0530

    .

--- pythonprograms/palindrome_sentance.py
+++ pythonprograms/palindrome_sentance.py
@@ -5,11 +5,35 @@ Created on Tue Mar 13 15:49:55 2018
 @author: HP
 """
 
-def palindrome_sentance_recog(a):
-    strip_sentance = ("".join(e for e in a if e.isalnum()))
-    strip_sentance = strip_sentance.lower()
-    if strip_sentance==strip_sentance[::-1]:
-        return True
-    else:
-        return False
-print(palindrome_sentance_recog('i  dood i'))
+#import urllib.request 
+##filename = 2
+
+#urllib.request.urlretrieve(url, filename)
+
+#import pandas as pd
+#df = pd.read_csv('c:/users/HP/Desktop/2.csv')
+#print(df.head())
+#
+#import requests
+#image_url = 'http://numerologystars.com/wp-content/uploads/2011/12/number-2-2.jpg'
+#img_data = requests.get(image_url).content
+#with open('image_name.jpg', 'wb') as handler:
+#    handler.write(img_data)
+#Import Python Imaging Library (PIL) 
+#import Image 
+from PIL import Image
+import numpy as np
+
+#pilimg = Image.open('2.jpg')
+#pilimg.save(“pil.jpg”)
+
+img = Image.open( '2.jpg')
+img.load()
+data = np.asarray( img, dtype="int32" )
+print(data)
+
+# Convert PIL image to numpy array 
+#arr = numpy.array(pilimg)
+
+#Convert numpy array to PIL image 
+#pilimg = Image.fromarray(arr)
--- pythonprograms/untitled0.py
+++ pythonprograms/untitled0.py
@@ -1,23 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar 12 21:08:14 2018
+Created on Mon Mar 19 16:27:15 2018
 
 @author: HP
 """
 
-class A:
-    com_name = "siva"
-#    name
-    
-    def __init__(self):
-        self.name = "bravi"
-        self.sal = 123.0
-#    @classmethod
-    def details(self):
-        print(A.com_name)
-        name = "ravi"
-        sal = 125.0
-        print(name,sal)
-        
-a = A()
-a.details()
+import json
+#with open('c:/users/HP/Desktop/enron.json') as data:
+f = open('c:/users/HP/Desktop/enron.json','r')
+s = f.read()
+#data = json.dumps(data)
+print(s)
+#t = json.load(s)
+#print(t)
